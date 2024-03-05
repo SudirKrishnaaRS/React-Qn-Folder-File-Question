191feba70cde4755f00bacae33d5f06ec9593732
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-03-05 17:23:22 +0530

    Basic Folder and File structure (Until 13:36 mins)

---
+++ README.md
@@ -0,0 +1,7 @@
+# FE Machine Coding Interview Question - Create a Folder amd File like working structure
+
+YT : https://youtu.be/20F_KzHPpvI?si=bdqNbT0NEpb41rkI
+
+Timepstamps and Commit :
+
+- 1) Commit 1 - Basic Folder and File structure (Until 13:36 mins)
---
+++ src/components/Folder.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
+function Folder({ explorer }) {
+  console.log(explorer);
+  
+  const [expand,setExpand] = useState(false);
+
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div className="folder" onClick={() => setExpand(!expand)}>
+          <span>📁 {explorer.name}</span>
+        </div>
+
+        <div style={{ display : expand ? "block" : "none", paddingLeft: 25}}>
+          {explorer.items.map((exp) => {
+            return <Folder explorer={exp} key={exp.id}/>
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">📄 {explorer.name}</span>;
+  }
+}
+
+export default Folder;
